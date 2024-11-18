5ce49e0f7e83e39b53fd6f32bcdf92ec3caf8a20
Andrey Egorov <and7643@gmail.com>
2024-11-18 15:32:01 +0300

    feat: mutliselect

--- lua/keys/common.lua
+++ lua/keys/common.lua
@@ -66,3 +66,4 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 })
 
+
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -99,5 +99,15 @@ return {
   },
   {
     "jiangmiao/auto-pairs",
+  },
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+	  init = function()
+	  	vim.g.VM_maps = {
+        ["Add Cursor Down"] = "<C-j>",
+        ["Add Cursor Up"] = "<C-k>",
+      }
+	  end,
   }
 }
