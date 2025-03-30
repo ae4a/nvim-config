cbb8ab7ae848926f4b5bccc14d8468a019b2d988
Andrey Egorov <and7643@gmail.com>
2025-03-30 11:57:40 +0300

    fix: clangd cmp

--- lua/plugins/lspconfigs/cpp.lua
+++ lua/plugins/lspconfigs/cpp.lua
@@ -1,6 +1,7 @@
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").clangd.setup({
+  cmd = {"clangd", "--enable-config", "--function-arg-placeholders=0"},
   filetypes = { 'c', 'h', 'cpp', 'hpp' },
   capabilities = lsp_capabilities,
 })
