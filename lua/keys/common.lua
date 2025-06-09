9c4533760c217419cf4ff578fcfa5b205d164abb
Andrey Egorov <and7643@gmail.com>
2025-06-09 19:53:32 +0300

    chore: hotkeys

--- lua/keys/common.lua
+++ lua/keys/common.lua
@@ -14,59 +14,49 @@ wk.add({
   },
   {
     mode = { "n" },
+    group = "buffer",
 
     -- Nvim tree
     { "<leader>e", "<cmd>NvimTreeFocus<cr>", desc = "Toggle nvim tree" },
+    { "<leader>E", "<cmd>NvimTreeClose<cr>", desc = "Close nvim tree" },
 
-    -- Telescope
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+    -- Telescope - do not use TODO remove telescope
+    -- { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+    -- { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope live grep" },
+    -- { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers" },
+    -- { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Telescope help tags" },
 
-    -- Bufferline
-    { "<C-Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Go to next buffer in bufferline" },
-    { "<C-S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Go to prev buffer in bufferline" },
+    -- Bufferline - do not use
+    -- { "<C-Tab>",    "<cmd>BufferLineCycleNext<cr>",  desc = "Go to next buffer in bufferline" },
+    -- { "<C-S-Tab>",  "<cmd>BufferLineCyclePrev<cr>",  desc = "Go to prev buffer in bufferline" },
 
     -- Buffer buy
-    { "<leader>q", "<cmd>:Bdelete<cr>", desc = "Delete current buffer" },
+    { "<leader>q",  "<cmd>:Bdelete<cr>",             desc = "Delete current buffer" },
 
-    -- To read errors
-    { "fj", "<cmd>:lua vim.diagnostic.open_float()<cr>", desc = "Show diagnostic" },
+    -- Fuzzy finder
+    { "<C-p>",      "<cmd>:FzfLua<cr>",              desc = "Fzf" },
+    { "<leader>g",      "<cmd>:FzfLua live_grep<cr>",    desc = "Fzf live grep" },
+    { "<leader>r",      "<cmd>:FzfLua live_grep_resume<cr>",    desc = "Fzf live grep resume" },
+
+    -- LSP
+    { "gh",         "<cmd>lua vim.lsp.buf.hover()<cr>",           desc = "Displays hover information about the symbol under the cursor" },
+    { "gd",         "<cmd>lua vim.lsp.buf.definition()<cr>",      desc = "Jump to the definition" },
+    { "gD",         "<cmd>lua vim.lsp.buf.declaration()<cr>",     desc = "Jump to declaration" },
+    { "gi",         "<cmd>lua vim.lsp.buf.implementation()<cr>",  desc = "Lists all the implementations for the symbol under the cursor" },
+    { "go",         "<cmd>lua vim.lsp.buf.type_definition()<cr>", desc = "Jumps to the definition of the type symbol" },
+    { "gr",         "<cmd>FzfLua lsp_workspace_diagnostics<cr>",  desc = "Lists all the references " },
+    { "gs",         "<cmd>lua vim.lsp.buf.signature_help()<cr>",  desc = "Displays a function's signature information" },
+    { "<F2>",       "<cmd>lua vim.lsp.buf.rename()<cr>",          desc = "Renames all references to the symbol under the cursor" },
+    { "<F4>",       "<cmd>lua vim.lsp.buf.code_action()<cr>",     desc = "Selects a code action available at the current cursor position" },
+    { "gl",         "<cmd>lua vim.diagnostic.open_float()<cr>",   desc = "Show diagnostics in a floating window" },
+    --{ "[d",   "<cmd>lua vim.diagnostic.goto_prev()<cr>",    desc = "Move to the previous diagnostic" },
+    --{ "]d",   "<cmd>lua vim.diagnostic.goto_next()<cr>",    desc = "Move to the next diagnostic" },
   },
   {
     mode = { "n", "t"},
 
     -- ToggleTerm
-    { "<C-o>", "<cmd>ToggleTerm size=10 dir=./ direction=horizontal name=Terminal<cr>", desc = "Toggle terminal" },
+    { "<C-o>", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle terminal" },
   }
 })
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function()
-
-    -- local opts = {buffer = true}
-    wk.add({
-      {
-        mode = {"n"},
-        group = "buffer",
-
-        { "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", desc = "Displays hover information about the symbol under the cursor" },
-        { "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", desc = "Jump to the definition" },
-        { "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", desc = "Jump to declaration" },
-        { "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", desc = "Lists all the implementations for the symbol under the cursor" },
-        { "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", desc = "Jumps to the definition of the type symbol" },
-        { "gr", "<cmd>lua vim.lsp.buf.references()<cr>", desc = "Lists all the references " },
-        { "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", desc = "Displays a function's signature information" },
-        { "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Renames all references to the symbol under the cursor" },
-        { "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Selects a code action available at the current cursor position" },
-        { "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", desc = "Show diagnostics in a floating window" },
-        { "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", desc = "Move to the previous diagnostic" },
-        { "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", desc = "Move to the next diagnostic" },
-      }
-    })
-  end
-})
-
 
